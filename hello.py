aebd2d07925ca66dc3021fe9dc09111dce326932
makaimark <makaimark@gmail.com>
2016-05-10 18:37:10 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 import sys
 
-name = input ( "Your name :" )
+	def main():
+	
+	name = input ( "Your name :" )
+	
+	if len(name) == 0 :
+		print ( " Hello world " )
+	else:
+		print ( " Hello " + name )
 
-if len(name) == 0 :
-	print ( " Hello world " )
-else:
-	print ( " Hello " + name )
-
-
+main()
